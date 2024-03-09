a91cd5daa7c3e25aec769382d7d29ccdb706fb11
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2024-03-09 18:42:11 +0100

    modified:   src/Diagnostica/Diagnostica.ps1 	modified:   "src/Sicurezza/ControlloVulnerabilit\303\240.ps1"

--- src/Diagnostica/Diagnostica.ps1
+++ src/Diagnostica/Diagnostica.ps1
@@ -1,3 +1,13 @@
+# Informazioni di sistema
+$systemInfo = @{
+    "Nome computer" = $env:COMPUTERNAME
+    "Nome utente" = $env:USERNAME
+    "Sistema operativo" = (Get-CimInstance Win32_OperatingSystem).Caption
+    "Versione sistema operativo" = (Get-CimInstance Win32_OperatingSystem).Version
+    "Architettura" = (Get-CimInstance Win32_OperatingSystem).OSArchitecture
+}
+$systemInfo | Format-Table -AutoSize
+
 # Dettagli della memoria RAM
 $MemoryInfo = Get-WmiObject Win32_PhysicalMemory | Select-Object DeviceLocator, Speed, Manufacturer, PartNumber, Capacity
 $MemoryInfo | Format-Table -AutoSize
@@ -13,3 +23,8 @@ $CPUInfo | Format-Table -AutoSize
 # Altre informazioni hardware
 $SystemInfo = systeminfo
 $SystemInfo | Select-String "Nome sistema operativo", "Produttore sistema", "Modello sistema", "Tipo sistema", "Totale RAM fisica", "Nome processore" | ForEach-Object { $_.ToString() }
+
+# Esegui systeminfo e salva l'output in una variabile
+$systemInfoOutput = Invoke-Expression -Command "systeminfo"
+# Visualizza l'output
+$systemInfoOutput | Format-Table -AutoSize
--- src/Sicurezza/ControlloVulnerabilità.ps1
+++ src/Sicurezza/ControlloVulnerabilità.ps1
@@ -1,30 +1,39 @@
-# Verifica se lo script viene eseguito con privilegi amministrativi
-$currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-$principal = New-Object System.Security.Principal.WindowsPrincipal($currentUser)
+try {
+    Write-Host "Ricerca delle vulnerabilità per il mese corrente."
 
-if ($principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Host "Lo script è in esecuzione con privilegi amministrativi."
-} else {
-    Write-Host "Lo script non è in esecuzione con privilegi amministrativi."
+    # Verifica se lo script viene eseguito con privilegi amministrativi
+    $currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object System.Security.Principal.WindowsPrincipal($currentUser)
+
+    if ($principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Write-Host "Lo script è in esecuzione con privilegi amministrativi."
+        Install-Module MSRCSecurityUpdates -Force 
+        Import-Module MSRCSecurityUpdates
+    }
+    else {
+        Write-Host "Lo script non è in esecuzione con privilegi amministrativi."
+        Install-Module MSRCSecurityUpdates -Force -Scope CurrentUser
+        Import-Module MSRCSecurityUpdates
+    }
+    $Month = Get-Date -Format 'yyyy-MMM'
+    $monthOfInterest = $Month
+
+    $CVRFDoc = Get-MsrcCvrfDocument -ID $monthOfInterest -Verbose
+    $CVRFHtmlProperties = @{
+        Vulnerability    = $CVRFDoc.Vulnerability 
+        ProductTree      = $CVRFDoc.ProductTree
+        DocumentTracking = $CVRFDoc.DocumentTracking
+        DocumentTitle    = $CVRFDoc.DocumentTitle
+    }
+    $fileName = "Microsoft-Bollettino-$Month.html"
+    $Output_Location = "$env:USERPROFILE\Downloads\$fileName"
+    Get-MsrcSecurityBulletinHtml -Verbose @CVRFHtmlProperties | Out-File $Output_Location
+}
+catch {
+    # Blocco di codice che gestisce l'eccezione
+    Write-Host "Si è verificato un errore:"
+    Write-Host $_.Exception.Message
     exit
 }
 
-# Installa il modulo MSRCSecurityUpdates se non è già installato
-if (-not (Get-Module -Name MSRCSecurityUpdates -ListAvailable)) {
-    Install-Module -Name MSRCSecurityUpdates -Force -Scope CurrentUser -Repository PSGallery
-}
-
-# Importa il modulo MSRCSecurityUpdates
-Import-Module -Name MSRCSecurityUpdates
-
-# Ottieni le informazioni sulle vulnerabilità relative al sistema corrente
-$vulnerabilities = Get-SecurityUpdate -Local -Detailed
-
-# Visualizza le informazioni sulle vulnerabilità
-if ($vulnerabilities) {
-    Write-Host "Elenco delle vulnerabilità relative al sistema corrente:"
-    $vulnerabilities | Format-Table -AutoSize
-} else {
-    Write-Host "Nessuna vulnerabilità trovata per il sistema corrente."
-}
-
+Write-Host "Rapporto del mese scaricato nella cartella Downloads."
