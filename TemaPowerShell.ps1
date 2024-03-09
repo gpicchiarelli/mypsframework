801fa7782a8a6efc1585cdccc44da7aa3a329ce9
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2024-03-09 12:58:18 +0100

    new file:   TemaPowerShell.ps1

---
+++ TemaPowerShell.ps1
@@ -0,0 +1,22 @@
+$ISETheme = @{
+    Command                  = $PSStyle.Foreground.FromRGB(0x0000FF)
+    Comment                  = $PSStyle.Foreground.FromRGB(0x006400)
+    ContinuationPrompt       = $PSStyle.Foreground.FromRGB(0x0000FF)
+    Default                  = $PSStyle.Foreground.FromRGB(0x0000FF)
+    Emphasis                 = $PSStyle.Foreground.FromRGB(0x287BF0)
+    Error                    = $PSStyle.Foreground.FromRGB(0xE50000)
+    InlinePrediction         = $PSStyle.Foreground.FromRGB(0x93A1A1)
+    Keyword                  = $PSStyle.Foreground.FromRGB(0x00008b)
+    ListPrediction           = $PSStyle.Foreground.FromRGB(0x06DE00)
+    Member                   = $PSStyle.Foreground.FromRGB(0x000000)
+    Number                   = $PSStyle.Foreground.FromRGB(0x800080)
+    Operator                 = $PSStyle.Foreground.FromRGB(0x757575)
+    Parameter                = $PSStyle.Foreground.FromRGB(0x000080)
+    String                   = $PSStyle.Foreground.FromRGB(0x8b0000)
+    Type                     = $PSStyle.Foreground.FromRGB(0x008080)
+    Variable                 = $PSStyle.Foreground.FromRGB(0xff4500)
+    ListPredictionSelected   = $PSStyle.Background.FromRGB(0x93A1A1)
+    Selection                = $PSStyle.Background.FromRGB(0x00BFFF)
+}
+Set-PSReadLineOption -Colors $ISETheme
+
