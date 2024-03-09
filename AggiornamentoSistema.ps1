a84ac89db641d2e307c44ff18064bb1fff2b9ef6
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2024-03-09 11:56:10 +0100

    modified:   AggiornamentoSistema.ps1

--- AggiornamentoSistema.ps1
+++ AggiornamentoSistema.ps1
@@ -3,3 +3,15 @@ Install-Module PSWindowsUpdate
 Get-WindowsUpdate
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll
 
+# Aggiorna tutte le applicazioni installate tramite Winget
+winget upgrade --all
+
+# Aggiorna PowerShell alla versione più recente
+Install-Module PowerShellGet -Force -AllowClobber
+Install-Module -Name PowerShellGet -Force
+Update-Module -Name PowerShellGet
+Install-Module -Name PackageManagement -Force -AllowClobber
+Install-Script -Name Install-ModuleFromGitHub
+Install-ModuleFromGitHub -Repository PsGet -ModuleName PsGet
+Update-Module -Name PsGet
+
