555de73737a87ad69f5fa807812cd79faa2a1ff0
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2024-03-21 18:07:01 +0100

    new file:   "src/Diagnostica/ControlloIntegrit\303\240Immagine.ps1"

---
+++ src/Diagnostica/ControlloIntegritàImmagine.ps1
@@ -0,0 +1,12 @@
+# Verifica se lo script viene eseguito come amministratore
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Lo script deve essere eseguito con privilegi di amministratore." -ForegroundColor Red
+    # Esci dallo script se non è stato eseguito come amministratore
+    exit
+}
+
+# Comando DISM per pulire e ripristinare l'immagine di distribuzione
+$command = "DISM.exe /Online /Cleanup-Image /RestoreHealth"
+
+# Esegui il comando
+Invoke-Expression -Command $command
