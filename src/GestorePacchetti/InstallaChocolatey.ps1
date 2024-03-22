5b349afcdc4c0456355198ed67ab39756536b236
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2024-03-22 22:42:06 +0100

    commenti

--- src/GestorePacchetti/InstallaChocolatey.ps1
+++ src/GestorePacchetti/InstallaChocolatey.ps1
@@ -4,5 +4,6 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
     # Esci dallo script se non è stato eseguito come amministratore
     exit
 }
+#Run Get-ExecutionPolicy. If it returns Restricted, then run Set-ExecutionPolicy AllSigned or Set-ExecutionPolicy Bypass -Scope Process.
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
