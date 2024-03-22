03752853c91db38a21d897f42bde7853fe7cfc7a
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2024-03-22 22:17:36 +0100

    new file:   src/GestorePacchetti/InstallaChocolatey.ps1

---
+++ src/GestorePacchetti/InstallaChocolatey.ps1
@@ -0,0 +1,8 @@
+# Verifica se lo script viene eseguito come amministratore
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Lo script deve essere eseguito con privilegi di amministratore." -ForegroundColor Red
+    # Esci dallo script se non è stato eseguito come amministratore
+    exit
+}
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
