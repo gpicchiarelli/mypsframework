4660ddaa4d76ebe9332047010f6fed3b44da0ac7
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2024-03-24 16:19:30 +0100

    Pull Github massivo - corretto parallelismo

--- src/GitHub/CloneTuttiRepository.ps1
+++ src/GitHub/CloneTuttiRepository.ps1
@@ -10,9 +10,21 @@ param (
 # Ottieni la lista di tutti i repository
 $repos = (Invoke-RestMethod -Uri "https://api.github.com/users/$username/repos" -Headers @{Authorization = "token $token"}).clone_url
 
+# Contatore per limitare il parallelismo
+$counter = 0
+
 # Clona ciascun repository nella cartella di destinazione specificata
-$repos | ForEach-Object {
-    $repoName = [System.IO.Path]::GetFileNameWithoutExtension($_)
+foreach ($repo in $repos) {
+    $repoName = [System.IO.Path]::GetFileNameWithoutExtension($repo)
     $destinationRepoPath = Join-Path -Path $destinationPath -ChildPath $repoName
-    Start-Process -NoNewWindow -FilePath git -ArgumentList "clone $_ $destinationRepoPath"
+    git clone $repo $destinationRepoPath
+
+    # Incrementa il contatore
+    $counter++
+
+    # Limita il parallelismo a 5
+    if ($counter -eq 5) {
+        Start-Sleep -Seconds 1
+        $counter = 0
+    }
 }
