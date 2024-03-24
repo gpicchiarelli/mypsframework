93e17d41e49985410be581b0ca8c92e3d991e062
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2024-03-24 15:32:08 +0100

    github clone massivo con destinazione specificata

--- src/GitHub/CloneTuttiRepository.ps1
+++ src/GitHub/CloneTuttiRepository.ps1
@@ -2,15 +2,17 @@ param (
     [Parameter(Mandatory=$true)]
     [string]$username,
     [Parameter(Mandatory=$true)]
-    [string]$token
+    [string]$token,
+    [Parameter(Mandatory=$true)]
+    [string]$destinationPath
 )
 
 # Ottieni la lista di tutti i repository
 $repos = (Invoke-RestMethod -Uri "https://api.github.com/users/$username/repos" -Headers @{Authorization = "token $token"}).clone_url
 
-# Clona ciascun repository
-#foreach ($repo in $repos) {
-#    git clone $repo
-#}
-
-Write-Output "$repos" | xargs -n 1 -P 4 git clone
+# Clona ciascun repository nella cartella di destinazione specificata
+$repos | ForEach-Object {
+    $repoName = [System.IO.Path]::GetFileNameWithoutExtension($_)
+    $destinationRepoPath = Join-Path -Path $destinationPath -ChildPath $repoName
+    Start-Process -NoNewWindow -FilePath git -ArgumentList "clone $_ $destinationRepoPath"
+}
